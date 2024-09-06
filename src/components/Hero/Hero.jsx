6031c0478b1632c8a7c51de97cd9238312ad8560
sujoyghoshal <sujoyghoshal.s@gmail.com>
2024-09-06 21:10:37 +0530

    adding

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,20 +1,28 @@
 import React from "react";
-
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
+  const downloadResume = () => {
+    window.open("https://drive.google.com/uc?export=download&id=1RRMXafT0rdmLpxowBn7jW7ut-6ih4hTR", "_blank");
+  };
+
   return (
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Sujoy</h1>
         <p className={styles.description}>
-        Hello, I'm Sujoy, a passionate and skilled Full Stack Developer with expertise in Node.js, JavaScript, and React.js.
-        With a strong foundation in both front-end and back-end technologies, I bring a holistic approach to web development!
+          Hello, I'm Sujoy, a passionate and skilled Full Stack Developer with expertise in Node.js, JavaScript, and React.js.
+          With a strong foundation in both front-end and back-end technologies, I bring a holistic approach to web development!
         </p>
-        <a href="mailto:sujoyghoshal.s@gmail.com" className={styles.contactBtn}>
-          Contact Me
-        </a>
+        <div className="side-div">
+          <a href="mailto:sujoyghoshal.s@gmail.com" className={styles.contactBtn}>
+            Contact Me
+          </a>
+          <a href="" className={styles.contactBtn} onClick={downloadResume}>
+            Resume
+          </a>
+        </div>
       </div>
       <img
         src={getImageUrl("hero/myphoto.png")}
